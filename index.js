1d9a4a0e054fefe6288afe09346d719c9e46e686
panharith <sethapanharith@gmail.com>
2025-09-05 13:48:59 +0700

    new update for folder panharith and main index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import userPanharithRouter from './src/panharith/routes/user.route.js';
 
 const app = express();
+app.use(cors({ origin: "*" }));
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/api/panharith', userPanharithRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
