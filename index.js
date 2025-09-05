bf6067a5c05e035f0ce1822f3d1d47aae48e7285
MohDev24 <keomonyoudom@gmail.com>
2025-09-05 11:25:57 +0700

    from oudom

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World from Keo Monyoudom!');
+    res.send('Hello, World 22!');
 });
 
 app.listen(PORT, () => {
