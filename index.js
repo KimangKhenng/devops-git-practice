fb64d7f6efb4e34f7c46e5f98a8be85a6425d672
KimangMac14 <kimangkheang@gmail.com>
2025-09-05 13:48:43 +0700

    Pages

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact Page');
+});
+
 app.use('/users', userRoutes);
 
 app.listen(port, () => {
